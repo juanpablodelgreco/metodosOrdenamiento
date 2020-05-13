f263405f6d54b97afd31130065028941688175a5
Juan Pablo Del Greco <juanpablo.delgreco.jp@gmail.com>
2020-05-13 16:33:00 -0300

    Modificaciones
    Se agreco quicksort y mejorado de insercion.

--- src/metodosOrdenamiento/Ordenamiento.java
+++ src/metodosOrdenamiento/Ordenamiento.java
@@ -43,130 +43,46 @@ public class Ordenamiento {
 	
 	//SENSIBLE-ESTABLE
 	//ordeno desde una posicion todos los anteriores
-	public void ordenamientoInsercion(int[] vec) {
-		int p, j;
-		int aux;
-		for (p = 1; p < vec.length; p++) { // desde el segundo elemento hasta
-			aux = vec[p]; // el final, guardamos el elemento y
-			j = p - 1; // empezamos a comprobar con el anterior
-			while ((j >= 0) && (aux < vec[j])) { // mientras queden posiciones y el // valor de aux sea menor que los
-				vec[j + 1] = vec[j]; // de la izquierda, se desplaza a
-				j--; // la derecha
-			}
-			vec[j + 1] = aux; // colocamos aux en su sitio
+	public void OrdenarPorInsercion(int[] vec) {
+		int i, j; // Índices sobre la lista.
+		int aux; // Variable auxiliar para el intercambio de valores.
+
+		for (i = 1; i < 100; i++) { // ¡Ojo! Empezamos en 1
+			aux = vec[i]; // Guardamos el valor a ordenar.
+			j = i - 1;
+			while ((j >= 0) && vec[j] > aux) // Siempre que sea mayor que aux.
+				// ordenamos de menor a mayor.
+				vec[j + 1] = vec[j--]; // Trasladamos el valor y movemos el indice.
+			vec[j + 1] = aux; // Ponemos el valor a ordenar en su sitio.
 		}
 	}
-	
-	
-	
-	
-	 public int partition(int arr[], int low, int high) 
-	    { 
-	        int pivot = arr[high];  
-	        int i = (low-1); // index of smaller element 
-	        for (int j=low; j<high; j++) 
-	        { 
-	            // If current element is smaller than the pivot 
-	            if (arr[j] < pivot) 
-	            { 
-	                i++; 
-	  
-	                // swap arr[i] and arr[j] 
-	                int temp = arr[i]; 
-	                arr[i] = arr[j]; 
-	                arr[j] = temp; 
-	            } 
-	        } 
-	  
-	        // swap arr[i+1] and arr[high] (or pivot) 
-	        int temp = arr[i+1]; 
-	        arr[i+1] = arr[high]; 
-	        arr[high] = temp; 
-	  
-	        return i+1; 
-	    } 
-	  
 	//NO SENSIBLE -> DEPENDE DE LA ELECCION DEL PIVOTE
 	//NO ESTABLE
 	 //O(n*log n)
-	 public void quickSort(int arr[], int low, int high) 
-	    { 
-	        if (low < high) 
-	        { 
-	            /* pi is partitioning index, arr[pi] is  
-	              now at right place */
-	            int pi = partition(arr, low, high); 
-	  
-	            // Recursively sort elements before 
-	            // partition and after partition 
-	            quickSort(arr, low, pi-1); 
-	            quickSort(arr, pi+1, high); 
-	        } 
-	    } 
-	 
-//	 public void sort(int arr[], int left, int right){
-//		    if(left < right){
-//		      //Encuentra el punto medio del vector.
-//		      int middle = (left + right) / 2;
-//		      
-//		      //Divide la primera y segunda mitad (llamada recursiva).
-//		      sort(arr, left, middle);
-//		      sort(arr, midddle+1, right);
-//
-//		      //Une las mitades.
-//		      merge(arr, left, middle, right);
-//		    }
-//		}
-//	 
-//	 public void merge(int arr[], int left, int middle, int right) {
-//		  //Encuentra el tamaño de los sub-vectores para unirlos.
-//		  int n1 = middle - left + 1;
-//		  int n2 = right - middle;
-//
-//		  //Vectores temporales.
-//		  int leftArray[] = new int [n1];
-//		  int rightArray[] = new int [n2];
-//
-//		  //Copia los datos a los arrays temporales.
-//		  for (int i=0; i < n1; i++) {
-//		    leftArray[i] = arr[left+i];
-//		  }
-//		  for (int j=0; j < n2; j++) {
-//		    rightArray[j] = arr[middle + j + 1];
-//		  }
-//		  /* Une los vectorestemporales. */
-//
-//		  //Índices inicial del primer y segundo sub-vector.
-//		  int i = 0, j = 0;
-//
-//		  //Índice inicial del sub-vector arr[].
-//		  int k = left;
-//
-//		  //Ordenamiento.
-//		  while (i < n1 && j < n2) {
-//		    if (leftArray[i] <= rightArray[j]) {
-//		      arr[k] = leftArray[i];
-//		      i++;
-//		    } else {
-//		        arr[k] = rightArray[j];
-//		        j++;
-//		    }
-//		    k++;
-//		  }//Fin del while.
-//
-//		  /* Si quedan elementos por ordenar */
-//		  //Copiar los elementos restantes de leftArray[].
-//		  while (i < n1) {
-//		    arr[k] = leftArray[i];
-//		    i++;
-//		    k++;
-//		  }
-//		  //Copiar los elementos restantes de rightArray[].
-//		  while (j < n2) {
-//		    arr[k] = rightArray[j];
-//		    j++;
-//		    k++;
-//		  }
-//		}
-	
+		public static void quicksort(int vec[], int izq, int der) {
+
+			int pivote = vec[izq]; // tomamos primer elemento como pivote
+			int i = izq; // i realiza la búsqueda de izquierda a derecha
+			int j = der; // j realiza la búsqueda de derecha a izquierda
+			int aux;
+
+			while (i < j) { // mientras no se crucen las búsquedas
+				while (vec[i] <= pivote && i < j)
+					i++; // busca elemento mayor que pivote
+				while (vec[j] > pivote)
+					j--; // busca elemento menor que pivote
+				if (i < j) { // si no se han cruzado
+					aux = vec[i]; // los intercambia
+					vec[i] = vec[j];
+					vec[j] = aux;
+				}
+			}
+			vec[izq] = vec[j]; // se coloca el pivote en su lugar de forma que tendremos
+			vec[j] = pivote; // los menores a su izquierda y los mayores a su derecha
+			if (izq < j - 1)
+				quicksort(vec, izq, j - 1); // ordenamos subarray izquierdo
+			if (j + 1 < der)
+				quicksort(vec, j + 1, der); // ordenamos subarray derecho
+		}
+
 }
